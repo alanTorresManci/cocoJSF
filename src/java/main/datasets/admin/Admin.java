922f544a0209e0b585b70260e850793031488bd6
alanTorresManci <alan.desarrollo@gmail.com>
2016-06-14 10:03:13 -0500

    lo salve

---
+++ src/java/main/datasets/admin/Admin.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.datasets.admin;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+/**
+ * 
+ *
+ * 
+ * 
+ * @author germanamz
+ */
+@ManagedBean(name = "Admin")
+@SessionScoped
+public class Admin implements Serializable {
+    
+    private String username;
+    private String password;
+    public EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("CocoPU");
+    public EntityManager em = emf.createEntityManager();
+    public Admin() {
+        
+    }
+
+    /*
+    Getters & Setters
+    */
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    /*
+    Pubic functions
+    */
+    public Object[] administradores(){
+        Admins admins = new Admins();
+        return admins.admin(em);
+    }
+            
+    public String login() {
+        Admins admins = new Admins();
+        
+        int result = admins.findByAll(this.username, this.password, em);
+        if(result == 1)
+        {
+            ExternalContext ec = FacesContext.getCurrentInstance()
+                    .getExternalContext();
+            try {
+                ec.redirect(ec.getRequestContextPath()
+                        + "/faces/index.xhtml");
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            
+        }
+        else
+            System.out.println("no");
+        return "";
+    }
+    
+}
---
+++ src/java/main/datasets/conference/Conference.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.datasets.conference;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author germanamz
+ */
+@ManagedBean(name = "Conference")
+@ApplicationScoped
+public class Conference implements Serializable {
+
+    private int id;
+    private String name;
+    private String exhibitor;
+    private int capacity;
+    private String date;
+    private String image;
+    private String synopsis;
+    private int value;
+    private String room;
+    
+    public EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("CocoPU");
+    public EntityManager em = emf.createEntityManager();
+    
+    public Conference() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExhibitor() {
+        return exhibitor;
+    }
+
+    public void setExhibitor(String exhibitor) {
+        this.exhibitor = exhibitor;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+    
+    public void storeConference(){
+        Conferences conferences = new Conferences();
+        conferences.store(em, name, exhibitor, capacity, date, synopsis, value, room);
+        ExternalContext ec = FacesContext.getCurrentInstance()
+                    .getExternalContext();
+            try {
+                ec.redirect(ec.getRequestContextPath()
+                        + "/faces/index.xhtml");
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        
+    }
+    public List<Conferences> getConferences()
+    {
+        Conferences conferences = new Conferences();
+        return conferences.getAll(em);
+    }
+    
+    public void setRoom(String room) {
+        this.room = room;
+    }
+    
+    
+    
+}
